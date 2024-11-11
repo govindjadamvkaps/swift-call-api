851c25e5a019045adb144a40a7c0c9d11bf2ce8f
vkaps <Monikagupta-100>
2024-11-11 15:37:14 +0500

    adding consoles

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.get("/", (req, res) => {
   return res
     .status(200)
-    .json({ success: true, message: "Socket API is running new code" });
+    .json({ success: true, message: "Socket API is running new code!!!" });
 });
 
 app.get("/api/socket", (req, res) => {
