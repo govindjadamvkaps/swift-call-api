692c738f6f427e3fa02e84fcef4f878404e69c30
govindjadamvkaps <govind.jadam@vkaps.com>
2024-12-03 11:21:16 +0530

    testing

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/user', UserRouter)
 app.get("/", (req, res) => {
   return res
     .status(200)
-    .json({ success: true, message: "Socket API is running new code!!" });
+    .json({ success: true, message: "Socket API is running new code!!!" });
 });
 
 app.get("/api/socket", (req, res) => {
